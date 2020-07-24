c08c9d141070b1931f07564bfbf77a4aa7744960
sfespinosa <scottjames.espinosa@gmail.com>
2020-07-24 14:32:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drank) {
+        this.food = food
+        this.drink = drank
+    }
+}
+
+class Lunch {
+    constructor(salad, food, drank){
+        this.salad = salad
+        this.soup = food
+        this.drink = drank
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._dessert = dessert
+    }
+}
